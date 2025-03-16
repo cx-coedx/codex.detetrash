5fcf37dad71d56a3b7cab442eeff26f4b30b9945
cx-coedx <soda10102003@gmail.com>
2025-03-16 18:18:28 +0700

    1 |

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+-----
+-- อย่านำไปขาย และ ขอเคดิตด้วยนะ
+
+
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Codex'
+
+files {
+    'stream/procedural.meta',
+    'stream/v_bush.ytyp',
+    'stream/v_rubbish.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/v_bush.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/v_rubbish.ytyp'
+data_file 'PROCEDURAL_SETTINGS_FILE' 'stream/procedural.meta'
